5f72625cb3f0a7bc8baab9096741c77ea6c4ec93
PonPonc <gybaez321@gmail.com>
2023-05-22 08:07:14 +0800

    [Update] Final Push for Forgot Password Logic and Design

--- G6FinalProjectPostly/G6FinalProjectPostly/Pages/ForgotPass.xaml.cs
+++ G6FinalProjectPostly/G6FinalProjectPostly/Pages/ForgotPass.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using G6FinalProjectPostly.Assets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,46 @@ namespace G6FinalProjectPostly.Pages
         public ForgotPass()
         {
             InitializeComponent();
+        }
+
+        private async void SendNewPassRequestBtn_Clicked(object sender, EventArgs e)
+        {
+            var usrEmail = user_email.Text;
+            List<Users> emailList = Users.UserList;
+            bool emailFound = false;
+
+            if(usrEmail != null ) 
+            {
+                if (usrEmail.ToLower().Contains('@') && usrEmail.ToLower().Contains('.'))
+                {
+                    foreach (var email in emailList)
+                    {
+                        if (email.Email() == usrEmail)
+                        {
+                            emailFound = true;
+                        }
+
+                    }
+                    if (emailFound == true)
+                    {
+                        await DisplayAlert("Success", "Your recovery email will be sent shortly.", "Ok");
+                        await Navigation.PushAsync(new MainPage());
+                    }
+                    else
+                    {
+                        await DisplayAlert("Fail", "The email does not exist!", "Ok");
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Alert", "Please use an email format for the entry.", "Close");
+                }
+
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Please do not leave the email field empty.", "Close");
+            }
         }
     }
 }
